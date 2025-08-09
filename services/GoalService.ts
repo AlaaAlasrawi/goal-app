c2986392d63076a752174672194a9b58c260ec71
AlaaAlasrawi <loloyousef2622003@gmail.com>
2025-08-09 22:52:38 +0300

    init

--- services/GoalService.ts
+++ services/GoalService.ts
@@ -1,17 +1,40 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Goal } from "../hooks/types";
 
+const URL = "http://192.168.1.165:8080/api/v1/goal";
+
 class GoalService {
   public async addGoal(goal: Goal): Promise<void> {
-    const goals: Goal[] = JSON.parse(
-      (await AsyncStorage.getItem("goals")) ?? "[]"
-    );
-    goals.push(goal);
-    await AsyncStorage.setItem("goals", JSON.stringify(goals));
+    try {
+      await fetch(URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(goal),
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   public async getAllGoals(): Promise<Goal[]> {
-    return JSON.parse((await AsyncStorage.getItem("goals")) ?? "[]");
+    try {
+      const promise = await fetch(URL, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const response = await promise.json();
+      console.log(response);
+
+      return response.content;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   }
 
   public async deleteGoal(goalId: number): Promise<void> {
